f1d51f26f16502567192e42c87e7f523ccb11f65
Juan <ramollino@gmail.com>
2019-07-03 17:53:09 -0300

    Removing wording

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -8,7 +8,7 @@ public class Application {
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-    	System.out.println ("Starting Application now!");
+    	System.out.println ("Starting Application");
 	Application app = new Application();
     }
 }
